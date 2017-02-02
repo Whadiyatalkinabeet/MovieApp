0bebeb4779eb9ff0f7034ed4bc928a07e98f1a83
jon <jpgmend@gmail.com>
2017-02-02 18:10:08 +0000

    index.php new releases test

--- MovieSite/Aggregator/index.php
+++ MovieSite/Aggregator/index.php
@@ -39,7 +39,7 @@
 	</body>
 	<script>
 		function NewReleases(){
-			var url = "https://api.themoviedb.org/3/discover/movie?api_key=6d64a72486b47e66eaf157cafc5a0860&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+			var url = "GetMovie.php";
 			var From="NewReleases";
 			window.history.pushState(null,null,'NewReleases');//Mostly for future extensibility 
 			$.getJSON(url, function(result){
