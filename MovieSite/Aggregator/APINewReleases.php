27593819716df6aa5c56b27b0f324f3f510bc230
jon <jpgmend@gmail.com>
2017-02-02 18:22:48 +0000

    test 2

--- MovieSite/Aggregator/APINewReleases.php
+++ MovieSite/Aggregator/APINewReleases.php
@@ -3,11 +3,10 @@
 	ini_set('display_errors',1);
 	error_reporting(E_ALL);
 	include'API.php';
-    /*$fcon = fopen($APINewReleases, "r"); //APINewReleases from included API.php
+    $fcon = fopen($APINewReleases, "r"); //APINewReleases from included API.php
     if ($fcon) {
         while (!feof($fcon)) $data_json .= fgets($fcon, 4096);
         fclose($fcon);
     }
-    echo $data_json;*/
-    echo $APINewReleases;
+    echo $data_json;
 ?>
