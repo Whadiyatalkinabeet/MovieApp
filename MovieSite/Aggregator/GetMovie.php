5a2a47dbc48d03eb97bb543577ed83bfc491ef84
jon <jpgmend@gmail.com>
2017-02-02 18:06:40 +0000

    getMovie test

--- MovieSite/Aggregator/GetMovie.php
+++ MovieSite/Aggregator/GetMovie.php
@@ -1,5 +1,5 @@
  <?php
-    $url = "https://api.themoviedb.org/3/discover/movie?api_key=6d64a72486b47e66eaf157cafc5a0860&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+    /*$url = "https://api.themoviedb.org/3/discover/movie?api_key=6d64a72486b47e66eaf157cafc5a0860&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
     $fcon = fopen($url, "r");
     if ($fcon) {
         while (!feof($fcon)) $data_json .= fgets($fcon, 4096);
@@ -11,5 +11,12 @@
     }
     else{
         echo "yes";
+    }*/
+    $url= "https://api.themoviedb.org/3/discover/movie?api_key=6d64a72486b47e66eaf157cafc5a0860&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+    $fcon = fopen($url, "r");
+    if ($fcon) {
+        while (!feof($fcon)) $data_json .= fgets($fcon, 4096);
+        fclose($fcon);
     }
+    echo $data_json;
 ?> 
