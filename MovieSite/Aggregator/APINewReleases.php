22d9ad58eb49347873000e973ced693be8714144
jon <jpgmend@gmail.com>
2017-02-02 18:20:35 +0000

    API testing 1

--- MovieSite/Aggregator/APINewReleases.php
+++ MovieSite/Aggregator/APINewReleases.php
@@ -3,11 +3,11 @@
 	ini_set('display_errors',1);
 	error_reporting(E_ALL);
 	include'API.php';
-    $fcon = fopen($APINewReleases, "r"); //APINewReleases from included API.php
+    /*$fcon = fopen($APINewReleases, "r"); //APINewReleases from included API.php
     if ($fcon) {
         while (!feof($fcon)) $data_json .= fgets($fcon, 4096);
         fclose($fcon);
     }
-    echo $data_json;
-
+    echo $data_json;*/
+    echo $APINewReleases;
 ?>
