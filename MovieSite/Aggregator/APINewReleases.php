e5c519ac6ede6f18513e8e8a32d407240f32a5c8
jon <jpgmend@gmail.com>
2017-02-02 18:25:35 +0000

    test 4

--- MovieSite/Aggregator/APINewReleases.php
+++ MovieSite/Aggregator/APINewReleases.php
@@ -1,7 +1,7 @@
 <?php
 //Callback for the NewReleases page
-	ini_set('display_errors',1);
-	error_reporting(E_ALL);
+	//ini_set('display_errors',1);
+	//error_reporting(E_ALL);
 	include'API.php';
     $fcon = fopen($APINewReleases, "r"); //APINewReleases from included API.php
     if ($fcon) {
